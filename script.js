d0efa10b4d52411a58e9ca1f55210be2c15f6eb8
Luan <9001545@riachuelo.com.br>
2022-03-08 08:13:28 -0300

    finished modal example

--- script.js
+++ script.js
@@ -3,9 +3,30 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for(let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i])
+const openModal = function() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden')
 }
+
+const closeModal =  function() {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
+
+const keydownCloseModal = function (e) {
+  const pressedKey = e.key
+  if(pressedKey === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal()
+  }
+}
+
+for(let i = 0; i < btnsOpenModal.length; i++) 
+  btnsOpenModal[i].addEventListener('click', openModal)
+
+  btnCloseModal.addEventListener('click', closeModal)
+
+  overlay.addEventListener('click', closeModal)
+
+  document.addEventListener('keydown', keydownCloseModal)
